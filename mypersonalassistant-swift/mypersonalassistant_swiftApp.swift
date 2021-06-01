8dcbca342eb62b3fb1045a377c9edaee38d37ea1
jonaslindebros <jonaslindebros@gmail.com>
2021-06-01 10:56:09 +0200

    init project

---
+++ mypersonalassistant-swift/mypersonalassistant_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  mypersonalassistant_swiftApp.swift
+//  mypersonalassistant-swift
+//
+//  Created by JonasL on 2021-06-01.
+//
+
+import SwiftUI
+
+@main
+struct mypersonalassistant_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
